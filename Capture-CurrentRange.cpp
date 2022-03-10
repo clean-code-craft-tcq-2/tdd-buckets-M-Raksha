e2de50d18c86405e461e2177306c3c4204c4e468
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-03-10 07:27:43 +0530

    Update Capture-CurrentRange.cpp

--- Capture-CurrentRange.cpp
+++ Capture-CurrentRange.cpp
@@ -1,2 +1,9 @@
 #include<iostream>
 using namespace std;
+
+string ContentToBeWrittenToCSV(int *currentsample, int numberOfSample)
+{
+    string ToCSVfile = to_string(currentsample[0])+"-"+to_string(currentsample[numberOfSample-1])+","+to_string(numberOfSample);
+    
+    return ToCSVfile;
+}
