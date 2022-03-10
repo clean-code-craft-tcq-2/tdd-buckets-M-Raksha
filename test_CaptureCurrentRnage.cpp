507617c3e2c01026fa299dda902983d780d6b096
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-03-10 07:37:35 +0530

    Update test_CaptureCurrentRnage.cpp

--- test_CaptureCurrentRnage.cpp
+++ test_CaptureCurrentRnage.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-#include "Capture-CurrentRange.h"
+#include "capture-CurrentRange.h"
 
 TEST_CASE("Finds what needs to be written to CSV file")
 {
