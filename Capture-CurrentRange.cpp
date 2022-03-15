4bea623c8e83475fa46e8e8f2519aff7e21f4d2c
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-03-15 20:57:03 +0530

    Update Capture-CurrentRange.cpp

--- Capture-CurrentRange.cpp
+++ Capture-CurrentRange.cpp
@@ -2,12 +2,18 @@
 #include "capture-CurrentRnage.h"
 using namespace std;
 
-
-
 std::string ContentToBeWrittenToCSV(std::array<std::vector <int>,2>currentsampleVector)
 {
-    string ToCSVfile = "4-5,2\n";
-    
-    return ToCSVfile;
+    string ToCSVfile = "";
+    string rangestring;
+    for(int i=0; i<2;i++)
+    {
+    if(currentsampleVector[i].size() != 0)
+    {
+    rangestring= to_string(currentsampleVector[i].front())+"-"+to_string(currentsampleVector[i].back())+","+to_string(currentsampleVector[i].size())+"\n";
+    ToCSVfile = ToCSVfile + rangestring;
+    }
+    }
+  return ToCSVfile;
 }
 
