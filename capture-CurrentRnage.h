1cf83e711490517f8530a96629d04909f8ab69f2
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-03-15 22:33:18 +0530

    Update capture-CurrentRnage.h

--- capture-CurrentRnage.h
+++ capture-CurrentRnage.h
@@ -3,3 +3,4 @@
 #include <array>
 
 std::string ContentToBeWrittenToCSV(std::array<std::vector <int>,2>currentsampleVector);
+std::array<std::vector <int>,2> findCurrentRange(int* arrayofCurrentSample,  int numberofsamples);
