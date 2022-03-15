883df075cc306012b491f8347149a8205db7ead2
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-03-15 20:54:31 +0530

    Update Capture-CurrentRange.cpp

--- Capture-CurrentRange.cpp
+++ Capture-CurrentRange.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 std::string ContentToBeWrittenToCSV(std::array<std::vector <int>,2>currentsampleVector)
 {
-    string ToCSVfile = "4-5,2";
+    string ToCSVfile = "4-5,2\n";
     
     return ToCSVfile;
 }
